43913380af81df2c6e0cd9c5522ca1356b67c0db
farah310 <78139056+farah310@users.noreply.github.com>
2021-03-15 10:40:29 +0300

    Add files via upload

---
+++ extstysheet.cs
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+p {
+  font-family: georgia, serif; 
+  font-size: x-large;
+  background color:antquewhite;
+  }
+a:phunky {
+  color: pink; 
+  text-decoration: none;
+  text-align:centered;
+  }
+</style>
+</head>
+<body>
+<p>This paragraph tag has been preset using embedded style sheets. Same for the horizontal rule below and the hyperlink under that. </p>
+<p>Also see <a href="/css/external_style_sheets.cfm" target="_blank">external style sheets</a></p>
+</body>
+</html>
